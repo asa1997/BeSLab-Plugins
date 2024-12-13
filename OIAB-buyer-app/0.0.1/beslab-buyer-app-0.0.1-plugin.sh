d88eebbaaebaca1c3664e482e3477f28e65d0cae
asa1997 <arunsureshampadath@gmail.com>
2024-12-13 14:40:55 +0530

    Added buyer app and buyer ui plugin

---
+++ OIAB-buyer-app/0.0.1/beslab-buyer-app-0.0.1-plugin.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+function install_docker() {
+
+	__besman_echo_no_colour "Checking if Docker is installed..."
+
+	if command -v docker >/dev/null 2>&1; then
+		__besman_echo_yellow "Docker is already installed"
+		docker --version
+	else
+		__besman_echo_white "Docker not found. Installing Docker..."
+
+		# Update package index
+		sudo apt-get update
+
+		# Install required packages
+		sudo apt-get install -y \
+			apt-transport-https \
+			ca-certificates \
+			curl \
+			gnupg \
+			lsb-release
+
+		# Add Docker's official GPG key
+		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+		# Set up stable repository
+		echo \
+			"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+          $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+
+		# Install Docker Engine
+		sudo apt-get update
+		sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+		# Start Docker service
+		sudo systemctl start docker
+		sudo systemctl enable docker
+
+		# Add current user to docker group
+		sudo groupadd docker
+		sudo usermod -aG docker $USER
+		# newgrp docker
+
+
+		__besman_echo_yellow "Docker installed successfully!"
+		docker --version
+	fi
+}
+
+function install_docker_compose() {
+	__besman_echo_no_colour "Checking if Docker Compose is installed..."
+
+	if command -v docker-compose >/dev/null 2>&1; then
+		__besman_echo_yellow "Docker Compose is already installed"
+		docker-compose --version
+	else
+		__besman_echo_white "Docker Compose not found. Installing latest version..."
+
+		# Download the latest version of Docker Compose directly from GitHub releases
+		sudo curl -L "https://github.com/docker/compose/releases/download/latest/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+		# check if operation successful
+		if [ $? -ne 0 ]; then
+			__besman_echo_red "Failed to download Docker Compose."
+			return 1
+		fi
+		# Apply executable permissions
+		sudo chmod +x /usr/local/bin/docker-compose
+
+		__besman_echo_yellow "Docker Compose installed successfully!"
+		docker-compose --version
+		if [ $? -ne 0 ]; then
+			__besman_echo_red "Failed to download Docker Compose."
+			return 1
+		fi
+	fi
+}
+
+
+function __beslab_install_buyer-app() {
+
+	local env_file="$BESLAB_OIAB_BUYER_APP_DIR/.env.default"
+    install_docker
+    install_docker_compose || return 1
+	if [[ -d $BESLAB_OIAB_BUYER_APP_DIR ]]; then
+		__besman_echo_white "Buyer app code found"
+	else
+		__besman_echo_white "Cloning source code repo from $BESLAB_ORG/$BESLAB_OIAB_BUYER_APP"
+		__besman_repo_clone "$BESLAB_ORG" "$BESLAB_OIAB_BUYER_APP" "$BESLAB_OIAB_BUYER_APP_DIR" || return 1
+	fi
+	cd "$BESLAB_OIAB_BUYER_APP_DIR" || return 1
+	# Check if MongoDB port is already mapped to 27018
+	if grep -q "27018:27017" docker-compose.yml; then
+		__besman_echo_yellow "MongoDB port already mapped to 27018"
+	else
+		sed -i 's/27017:27017/27018:27017/' docker-compose.yml
+		__besman_echo_white "Updated MongoDB port mapping to 27018:27017"
+	fi
+
+    if grep -q "$BESLAB_OIAB_BUYER_APP_PORT:$BESLAB_OIAB_BUYER_APP_PORT" docker-compose.yml; then
+		__besman_echo_yellow "Buyer app port already mapped to $BESLAB_OIAB_BUYER_APP_PORT"
+	else
+		sed -i "s/$BESLAB_OIAB_BUYER_APP_PORT:$BESLAB_OIAB_BUYER_APP_PORT/$BESLAB_OIAB_BUYER_APP_PORT:$BESLAB_OIAB_BUYER_APP_PORT/" docker-compose.yml
+		__besman_echo_white "Updated  port mapping to $BESLAB_OIAB_BUYER_APP_PORT:$BESLAB_OIAB_BUYER_APP_PORT"
+	fi
+
+	sed -i "s|PROTOCOL_SERVER_URL=.*|PROTOCOL_SERVER_URL=$BESLAB_IP_ADDRESS:5001|g" "$env_file"
+	sed -i "s|BAP_ID=.*|BAP_ID=$BESLAB_BAP_ID|g" "$env_file"
+	sed -i "s|BAP_URI=.*|BAP_URI=$BESLAB_BAP_URI|g" "$env_file"
+
+	__besman_echo_white "Installing $BESLAB_OIAB_BUYER_APP"
+	__besman_echo_yellow "Building buyer app"
+	sudo docker-compose up --build -d
+	cd "$HOME" || return 1
+}
---
+++ OIAB-buyer-ui/0.0.1/beslab-buyer-ui-0.0.1-plugin.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+function install_docker() {
+
+	__besman_echo_no_colour "Checking if Docker is installed..."
+
+	if command -v docker >/dev/null 2>&1; then
+		__besman_echo_yellow "Docker is already installed"
+		docker --version
+	else
+		__besman_echo_white "Docker not found. Installing Docker..."
+
+		# Update package index
+		sudo apt-get update
+
+		# Install required packages
+		sudo apt-get install -y \
+			apt-transport-https \
+			ca-certificates \
+			curl \
+			gnupg \
+			lsb-release
+
+		# Add Docker's official GPG key
+		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+		# Set up stable repository
+		echo \
+			"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+          $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+
+		# Install Docker Engine
+		sudo apt-get update
+		sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+		# Start Docker service
+		sudo systemctl start docker
+		sudo systemctl enable docker
+
+		# Add current user to docker group
+		sudo groupadd docker
+		sudo usermod -aG docker $USER
+		# newgrp docker
+
+
+		__besman_echo_yellow "Docker installed successfully!"
+		docker --version
+	fi
+}
+
+function __beslab_install_buyer-ui() {
+	install_docker
+	if [[ -d $BESLAB_OIAB_BUYER_UI_DIR ]]; then
+		__besman_echo_white "Buyer ui code found"
+	else
+		__besman_echo_white "Cloning source code repo from $BESLAB_ORG/$BESLAB_OIAB_BUYER_UI"
+		__besman_repo_clone "$BESLAB_ORG" "$BESLAB_OIAB_BUYER_UI" "$BESLAB_OIAB_BUYER_UI_DIR" || return 1
+	fi
+	cd "$BESLAB_OIAB_BUYER_UI_DIR" || return 1
+	__besman_echo_white "Installing $BESLAB_OIAB_BUYER_UI"
+	__besman_echo_yellow "Building OIAB buyer ui"
+
+	# Check Dockerfile port
+	if grep -q "EXPOSE $BESLAB_OIAB_BUYER_UI_PORT" Dockerfile; then
+		__besman_echo_yellow "Port $BESLAB_OIAB_BUYER_UI_PORT already exposed in Dockerfile"
+	else
+		sed -i "s/EXPOSE 80/EXPOSE $BESLAB_OIAB_BUYER_UI_PORT/g" Dockerfile
+		__besman_echo_white "Updated Dockerfile port to $BESLAB_OIAB_BUYER_UI_PORT"
+	fi
+
+	# Check nginx.conf port
+	if grep -q "listen $BESLAB_OIAB_BUYER_UI_PORT;" nginx.conf; then
+		__besman_echo_yellow "Port $BESLAB_OIAB_BUYER_UI_PORT already configured in nginx.conf"
+	else
+		sed -i "s/listen 80;/listen $BESLAB_OIAB_BUYER_UI_PORT;/g" nginx.conf
+		__besman_echo_white "Updated nginx.conf port to $BESLAB_OIAB_BUYER_UI_PORT"
+	fi
+
+	sudo docker build --build-arg VITE_API_BASE_URL="$BESLAB_IP_ADDRESS:$BESLAB_OIAB_BUYER_APP_PORT" -t oiab-buyer-ui .
+	sudo docker run -d --name oiab-buyer-ui -p $BESLAB_OIAB_BUYER_UI_PORT:$BESLAB_OIAB_BUYER_UI_PORT oiab-buyer-ui
+	cd "$HOME" || return 1
+}
+
+function __beslab_uninstall_buyer-ui()
+{
+	__besman_echo_yellow "Stopping and removing buyer ui"
+	sudo docker stop oiab-buyer-ui
+	sudo docker image rm oiab-buyer-ui
+
+	if [[ -d $BESLAB_OIAB_BUYER_UI_DIR ]]; then
+		rm -rf "$BESLAB_OIAB_BUYER_UI_DIR"
+	fi
+}
